2ae5efe2a86ee16258cfcf0e49367145b4c43323
CHENG-YU HAN <koreal6803@gmail.com>
2020-11-11 21:03:55 +0800

    fix(strategy_test): test function duplicated

--- tests/strategy_test.py
+++ tests/strategy_test.py
@@ -71,7 +71,7 @@ class TestStrategyMethods(unittest.TestCase):
         self.assertEqual(len(fig), 0)
 
 
-    def test_strategy_witht_setting_params(self):
+    def test_strategy_with_setting_params(self):
         @Strategy(n1=20, n2=30)
         def sma_strategy(ohlcv):
             sma1 = ohlcv.close.rolling(sma_strategy.n1).mean()
@@ -94,7 +94,7 @@ class TestStrategyMethods(unittest.TestCase):
         self.assertEqual((exits_signal != exits).all(), False)
         self.assertEqual(len(fig), 0)
 
-    def test_strategy_witht_setting_params(self):
+    def test_strategy_with_signle_params(self):
         @Strategy(n1=20, n2=30)
         def sma_strategy(ohlcv):
             sma1 = ohlcv.close.rolling(sma_strategy.n1).mean()
@@ -110,6 +110,24 @@ class TestStrategyMethods(unittest.TestCase):
         exits_signal = (sma1 > sma2).fillna(False)
 
         svars = {'n1': 10, 'n2': 20}
+
+        import vectorbt as vbt
+        portfolio = sma_strategy.backtest(ohlcv, variables=svars, freq='4h', plot=True)
+        self.assertEqual(isinstance(portfolio, vbt.Portfolio), True)
+
+    def test_strategy_with_multi_params(self):
+        @Strategy(n1=20, n2=30)
+        def sma_strategy(ohlcv):
+            sma1 = ohlcv.close.rolling(sma_strategy.n1).mean()
+            sma2 = ohlcv.close.rolling(sma_strategy.n2).mean()
+            return (sma1 > sma2), (sma2 > sma1)
+
+        ohlcv = self.ohlcv
+
+        sma1 = ohlcv.close.rolling(10).mean()
+        sma2 = ohlcv.close.rolling(20).mean()
+
+        svars = {'n1': [10, 11, 12], 'n2': 20}
 
         import vectorbt as vbt
         portfolio = sma_strategy.backtest(ohlcv, variables=svars, freq='4h', plot=True)
