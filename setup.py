7186b1c3aa489aad48a01585e128180922e53271
minggnim <38852608+minggnim@users.noreply.github.com>
2021-10-21 15:14:15 +0100

    update vectorbt version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         'pandas==1.1.5',
         'python-binance==0.7.5',
         'pyecharts==1.7.1',
-        'vectorbt==0.14.4',
+        'vectorbt==0.21.0',
         'statsmodels>=0.10.2',
         'tqdm>=4.41.1',
         'seaborn==0.10.1',
